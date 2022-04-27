4e59626e4a0cf45b3a824f7209e146b709d77b5a
ᴍᴜʜᴀᴍᴍᴇᴅ ᴛᴀʏʏɪʙ Şᴀʜɪɴ <muhammedtayyibsahin@yandex.com>
2022-04-27 22:48:03 +0300

    main

--- header.php
+++ header.php
@@ -41,7 +41,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="#" class="nav-link">Contact</a>
       </li>
     </ul>
-
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
--- setconfig.php
+++ setconfig.php
@@ -1,6 +1,25 @@
 <?php 
 
-if(!isset($_SESSION['admins'])){
+require_once 'connect/classCrud.php';
+$db=new Crud();
+
+
+if (!isset($_SESSION['admins']) && isset($_COOKIE['adminsLogin'])) {
+
+    $adminsLogin=json_decode($_COOKIE['adminsLogin']);
+
+    $sonuc=$db->adminsLogin($adminsLogin->admins_username,openssl_decrypt($adminsLogin->admins_pass,"AES-128-ECB","admins-coz"),true);
+
+    if ($sonuc['status']) {
+         
+        header("Location:index.php");
+    }
+}
+    else {
+
+}
+
+if(!isset($_SESSION['admins']) && !isset($_COOKIE['adminsLogin'])){
 
     header('Location:login.php');
     exit;
